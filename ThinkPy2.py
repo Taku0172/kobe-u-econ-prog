a8174492c492db356fb248242ab298b9bef1da46
Taku0172 <taku_0614@icloud.com>
2026-06-10 13:43:20 +0000

    update ThinkPy2.py

---
+++ ThinkPy2.py
@@ -0,0 +1,20 @@
+#変数の作成には、代入文を書く
+n = 17
+pi = 3.141592
+message = "And now for something completely different" #文字列も値になる
+
+print(message) #変数はそのまま使用可能
+print(n + 25)
+print(2 * pi)
+
+print(round(pi)) #関数の引用にも変数を使用可能
+
+import math #数学関数を集めたmathモジュールを読み込む
+
+print(math.pi) #円周率を取り出す
+
+print(math.sqrt(25)) #平方根を計算
+
+
+
+
