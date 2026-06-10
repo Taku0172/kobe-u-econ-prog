2e77c57a69dbe2f772bec6fb20141b034a8c34cd
Taku0172 <taku_0614@icloud.com>
2026-06-10 22:22:48 +0900

    Update ThinkPy1.py

--- ThinkPy1.py
+++ ThinkPy1.py
@@ -1,7 +1,21 @@
-30 + 12 #加算
+print(30 + 12) #加算
+
 print(43 - 1) #減算
+
 print(6 * 7) #乗算
+
 print(84 / 2) #除算、浮動小数点で返す
+
 print(85 // 2) #整数除算、小さい方の整数へ丸める、42.5にはならない！
+
 print(7 ** 2) #べき乗
-print
+
+print(round(42.4)) #最も近い整数に丸める
+
+print(abs(-42)) #絶対値を返す
+
+print("it's a small")
+
+print("Well, ")
+
+print("Well, " + "it's a small" + "world.")
